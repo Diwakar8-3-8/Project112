cfe958d2edf60c1b44522b9ba854b7d733765249
Diwakar8-3-8 <71541966+Diwakar8-3-8@users.noreply.github.com>
2021-06-25 10:34:22 +0530

    Add files via upload

---
+++ next.py
@@ -0,0 +1,7 @@
+all_savings = []
+for data in savings_data:
+  all_savings.append(float(data[0]))
+
+print(f"Mean of savings - {statistics.mean(all_savings)}")
+print(f"Median of savings - {statistics.median(all_savings)}")
+print(f"Mode of savings - {statistics.mode(all_savings)}")
